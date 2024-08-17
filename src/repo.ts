8d17333c9967ec6423c6af9b462af2440e4f2eff
gjsjohnmurray <johnm@georgejames.com>
2024-08-17 11:29:03 +0300

    Various fixes and enhancements

--- src/repo.ts
+++ src/repo.ts
@@ -1,14 +1,36 @@
+import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types';
 import { Octokit } from '@octokit/rest';
 import * as vscode from 'vscode';
+import * as git from '../git';
+
+const octokit = new Octokit();
+
+type GetBranchResponseDataType = GetResponseDataTypeFromEndpointMethod<typeof octokit.repos.getBranch>;
+type ListBranchesResponseDataType = GetResponseDataTypeFromEndpointMethod<typeof octokit.repos.listBranches>;
 
 export class Repositories {
 
-    async getrepoName(){
+    private git: git.API | undefined;
+
+    constructor() {
+        // Make the built-in Git extension's API available
+        this.git = vscode.extensions.getExtension<git.GitExtension>('vscode.git')?.exports?.getAPI(1);
+    }
+
+    getRepoName(){
         const workspaceFolders = vscode.workspace.workspaceFolders;
         if (workspaceFolders && workspaceFolders.length > 0) {
             return workspaceFolders[0].name;
         }
         return "Unknown";
+    }
+
+    getCurrentBranchName(): string{
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (workspaceFolders && workspaceFolders.length > 0) {
+            return this.git?.getRepository(workspaceFolders[0].uri)?.state.HEAD?.name ?? "";
+        }
+        return "";
     }
     
     async getParentName(userInfo: any, octokit: Octokit, repoName: any){
@@ -17,7 +39,7 @@ export class Repositories {
         try {
             parentRepo = await octokit.repos.get({ owner: userInfo.data.login, repo: repoName });
             if (parentRepo.data.parent) {
-                return parentRepo.data.parent.full_name
+                return parentRepo.data.parent.full_name;
             }
         } catch (err) {
             console.log(err);
@@ -25,18 +47,16 @@ export class Repositories {
         return;
     }
     
-    async getBranchList(userInfo: any, octokit: Octokit) {
-        let branchList;
-        const repoName = await this.getrepoName();
-          
-        try {
-            branchList = await octokit.repos.listBranches({ owner: userInfo.data.login, repo: repoName, per_page: 100 });
-            return branchList;
+    async getBranchList(userInfo: any, octokit: Octokit): Promise<ListBranchesResponseDataType> {
+        let branchList: ListBranchesResponseDataType;
+        const repoName = this.getRepoName();
+        const currentBranchName = this.getCurrentBranchName();
+        let currentBranch: GetBranchResponseDataType;
+        currentBranch = (await octokit.repos.getBranch({ owner: userInfo.data.login, repo: repoName, branch: currentBranchName })).data;
+        branchList = (await octokit.repos.listBranches({ owner: userInfo.data.login, repo: repoName, per_page: 100 })).data ?? [];
 
-        } catch (err) {
-            console.log(err);
-        }
-        return branchList;
+        // Put current branch first in the list
+        return [ currentBranch, ...branchList?.filter((branch: any) => branch.name !== currentBranchName) ];
     }
 
     async syncGitHubRepo(repo: any, branch: any, userInfo: any, octokit: Octokit) {
@@ -51,7 +71,7 @@ export class Repositories {
             });
             if (result) {
                 if (result.status === 200) {
-                    message = `Your branch ${branch} has been sync with ${repo}`;
+                    message = `The '${branch}' branch of your '${repo}' fork has been synced with its upstream`;
                 }
             }
         } catch (err: any) {
@@ -62,12 +82,12 @@ export class Repositories {
     }
     async handleQuickPickList(userInfo: any, octokit: Octokit) {
         const branchList = await this.getBranchList(userInfo, octokit);
-        const repoName = await this.getrepoName();
+        const repoName = this.getRepoName();
         const parentRepo = await this.getParentName(userInfo, octokit, repoName);
         try {
             if (branchList && parentRepo) {
                 // add a theme icon to quick pick item 
-                const items: vscode.QuickPickItem[] = branchList.data.map(({ name }) => ({
+                const items: vscode.QuickPickItem[] = branchList.map(({ name }) => ({
                     label: `$(git-branch) ${name}`,
                     description: name,
                 }));
@@ -78,7 +98,7 @@ export class Repositories {
                         return;
                     }
                     vscode.window
-                        .showInformationMessage(`Sync ${selection.description} from upsrteam ${parentRepo}`, "Sync", "Cancel")
+                        .showInformationMessage(`Sync the '${selection.description}' branch of your GitHub fork with its upstream '${parentRepo}'?`, { modal: true }, "Sync")
                         .then(answer => {
                             if (answer === `Sync`) {
                                 this.syncGitHubRepo(repoName, selection.description, userInfo, octokit);
@@ -86,7 +106,7 @@ export class Repositories {
                         });
                 });
             } else {
-                vscode.window.showInformationMessage(`Your repo ${repoName} doesn't have any upstream dependencies.`);
+                vscode.window.showInformationMessage(`Your repo ${repoName} doesn't have any upstream.`);
             }
         } catch (err) {
             console.log(err);
